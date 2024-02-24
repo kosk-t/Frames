19faaa72c1c117ddca8dbe0fcd222b13f25fa481
Kosk <crying.pierrot@gmail.com>
2024-02-25 03:03:29 +0900

    dynamic test

--- a-frame-in-100-lines-main/app/register/page.tsx
+++ a-frame-in-100-lines-main/app/register/page.tsx
@@ -8,21 +8,21 @@ import { v4 as uuidv4 } from 'uuid';
 import { Giveaway } from '../types';
 const prisma = new PrismaClient()
 
-export async function handleSubmit(data: Giveaway, formData: FormData) {
-  const guid = uuidv4();
-  // console.log(title)
-  const result = prisma.giveaway.create({
-    data:{
-      guid: guid,
-      title: data.title,
-      link:data.link,
-      linklabel:data.linkLabel,
-      closed:false
-    }  
-  });
-  const resultGuid = (await result).guid
-  redirect("/?guid=" + resultGuid);
-}
+// export async function handleSubmit(data: Giveaway, formData: FormData) {
+//   const guid = uuidv4();
+//   // console.log(title)
+//   const result = prisma.giveaway.create({
+//     data:{
+//       guid: guid,
+//       title: data.title,
+//       link:data.link,
+//       linklabel:data.linkLabel,
+//       closed:false
+//     }  
+//   });
+//   const resultGuid = (await result).guid
+//   redirect("/?guid=" + resultGuid);
+// }
 
 export default async function Page() {
   // console.log("test")
